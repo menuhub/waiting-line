176b147a64ca8d6764a44bf33e995bb1c6b3f6b5
Fabricio C Zuardi <fabricio@fabricio.org>
2020-02-29 13:02:42 -0300

    form, code and data adjusts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,69 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <form name="beta" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="beta" />
-      <p>
-        <label>
-          Email: <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <input type="checkbox" id="notification" name="notify" />
-        <label htmlFor="notification">Notify me when site launches.</label>
-      </p>
-      <p>
-        <input type="checkbox" id="earlyAccess" name="invite" />
-        <label htmlFor="earlyAccess">
-          I want to have early access to the preview.
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-  </Layout>
+const IndexPage = ({ data }) => {
+  const { title, description } = data.site.siteMetadata
+  return (
+    <Layout>
+      <div style={{ display: "flex" }}>
+        <div>
+          <WelcomeMessage title={title} description={description} />
+        </div>
+        <div>
+          <BetaForm />
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
+
+const WelcomeMessage = ({ title, description }) => (
+  <>
+    <p>
+      You have reached the homepage of <strong>{title}</strong>, {description}.
+    </p>
+    <p>
+      If you want to be notified when the project launches, or get early-access
+      to the preview, use the form.
+    </p>
+  </>
+)
+
+const BetaForm = () => (
+  <form name="beta" method="POST" data-netlify="true">
+    <input type="hidden" name="form-name" value="beta" />
+    <p>
+      <label>
+        Email: <input type="text" name="name" />
+      </label>
+    </p>
+    <p>
+      <input type="checkbox" id="notification" name="notify" />
+      <label htmlFor="notification">Notify me when site launches.</label>
+    </p>
+    <p>
+      <input type="checkbox" id="earlyAccess" name="invite" />
+      <label htmlFor="earlyAccess">
+        I want to have early access to the preview.
+      </label>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
 )
 
 export default IndexPage
